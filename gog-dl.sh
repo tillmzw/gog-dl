2150601812ab4bc94d7a3020e22ce9c0198972b4
Till Meyer zu Westram <till@meyerzuwestram.net>
2021-06-18 14:17:16 +0200

    don't try to repair files

--- gog-dl.sh
+++ gog-dl.sh
@@ -2,11 +2,11 @@
 
 
 lgogdownloader \
+	--threads 1 \
 	--directory /mnt/games/gog \
 	--platform linux,windows \
 	--language en \
 	--include installers,dlcs \
 	--save-changelogs \
 	--no-color \
-	--download \
-	--repair
+	--download
