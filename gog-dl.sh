43ced366f61ee91dbdce255e79250cd80f280925
Till Meyer zu Westram <till@meyerzuwestram.net>
2021-06-19 13:29:26 +0200

    download linux AND windows, remove old versions and empty dirs

--- gog-dl.sh
+++ gog-dl.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
+set -e
+
+declare -r DIR=/mnt/games/gog
 
 lgogdownloader \
 	--threads 1 \
-	--directory /mnt/games/gog \
-	--platform linux,windows \
+	--directory $DIR \
+	--platform linux+windows \
 	--language en \
 	--include installers,dlcs \
 	--save-changelogs \
 	--no-color \
-	--download
+	--download $@ \
+	2>&1 | tee $DIR/download.log
+
+lgogdownloader \
+	--directory $DIR \
+	--check-orphans \
+	| xargs -r rm -v 2>&1 | tee $DIR/orphans.log
+
+# remove empty dirs
+find $DIR -empty -type d -delete
